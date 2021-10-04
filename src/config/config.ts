86646c4a806d0923e1ff33a3e08938d903e60545
kigpand <kigpand@naver.com>
2021-10-04 23:30:21 +0900

    url 코드 오류

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-export const url = process.env.NODE_ENV === 'production' ? 'https://ji-todolist-server.herokuapp.com/' : 'http://localhost:8080';
+export const url = process.env.NODE_ENV === 'production' ? 'https://ji-todolist-server.herokuapp.com' : 'http://localhost:8080';
 
 // https://ji-todolist-server.herokuapp.com/
