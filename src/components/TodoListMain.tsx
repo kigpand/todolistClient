e4ec61309f1c341b2f7b45f0195ee3855215df84
kigpand <kigpand@naver.com>
2021-10-05 13:15:10 +0900

    title 수정

--- src/components/TodoListMain.tsx
+++ src/components/TodoListMain.tsx
@@ -132,7 +132,7 @@ const TodoListMain = ({ onOpenDialog }: Props) =>{
         <MainWrapper>
             <div className="body">
                 <div className = "title">
-                    {todoList.date.getFullYear()}년 {todoList.date.getMonth()+1}월 {todoList.date.getDate()}일
+                    { todoList.date && <div>{todoList.date.getFullYear()}년 {todoList.date.getMonth()+1}월 {todoList.date.getDate()}일</div>}
                     <div className = "line" />
                 </div>
                 <div className="lists">
